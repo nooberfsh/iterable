6714112ea20879e53313fb573b633e43c9ead47b
nooberfsh <nooberfsh@gmail.com>
2020-09-16 21:01:13 +0800

    add IterableMap

--- src/iterable.rs
+++ src/iterable.rs
@@ -1,8 +1,9 @@
+use std::collections::{HashMap, HashSet};
 use std::iter::FromIterator;
-use std::collections::HashSet;
 
 pub trait Iterable: IntoIterator {
-    type Collection<U>;
+    type C: FromIterator<Self::Item> = Self;
+    type CC<U>;
 
     fn count(self) -> usize
     where
@@ -11,28 +12,48 @@ pub trait Iterable: IntoIterator {
         self.into_iter().count()
     }
 
-    fn filter_map<U>(self, f: impl Fn(Self::Item) -> Option<U>) -> Self::Collection<U>
+    fn filter_map<U>(self, f: impl Fn(Self::Item) -> Option<U>) -> Self::CC<U>
     where
         Self: Sized,
-        Self::Collection<U>: FromIterator<U>
+        Self::CC<U>: FromIterator<U>,
     {
         self.into_iter().filter_map(f).collect()
     }
 
     // reduction
-    fn filter(self, f: impl Fn(&Self::Item) -> bool) -> Self::Collection<Self::Item>
+    fn filter(self, f: impl Fn(&Self::Item) -> bool) -> Self::C
     where
         Self: Sized,
-        Self::Collection<Self::Item>: FromIterator<Self::Item>
     {
         self.into_iter().filter(f).collect()
     }
 
     // transformation
-    fn map<U>(self, f: impl Fn(Self::Item) -> U) -> Self::Collection<U>
+    fn map<U>(self, f: impl Fn(Self::Item) -> U) -> Self::CC<U>
     where
         Self: Sized,
-        Self::Collection<U>: FromIterator<U>
+        Self::CC<U>: FromIterator<U>,
+    {
+        self.into_iter().map(f).collect()
+    }
+
+}
+
+pub trait IterableMap<K, V>: Iterable<Item = (K, V)> {
+    type CCMap<X, Y>;
+
+    fn map_value<U>(self, f: impl Fn(V) -> U) -> Self::CCMap<K, U>
+    where
+        Self: Sized,
+        Self::CCMap<K, U>: FromIterator<(K, U)>,
+    {
+        self.into_iter().map(|(k, v)| (k, f(v))).collect()
+    }
+
+    fn map_kv<X, Y>(self, f: impl Fn((K, V)) -> (X, Y)) -> Self::CCMap<X, Y>
+    where
+        Self: Sized,
+        Self::CCMap<X, Y>: FromIterator<(X, Y)>,
     {
         self.into_iter().map(f).collect()
     }
@@ -42,17 +63,24 @@ pub trait Iterable: IntoIterator {
 // iterable implementations
 
 impl<T> Iterable for Vec<T> {
-    type Collection<U> = Vec<U>;
+    type CC<U> = Vec<U>;
 }
 
 impl<'a, T: 'a> Iterable for &[T] {
-    type Collection<U> = Vec<U>;
+    type CC<U> = Vec<U>;
 }
 
 impl<T> Iterable for HashSet<T> {
-    type Collection<U> = HashSet<T>;
+    type CC<U> = HashSet<T>;
 }
 
+impl<K, V> Iterable for HashMap<K, V> {
+    type CC<U> = Vec<U>;
+}
+
+impl<K, V> IterableMap<K, V> for HashMap<K, V> {
+    type CCMap<X, Y> = HashMap<X, Y>;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // iterable implementations for reference
@@ -62,17 +90,16 @@ where
     I: Iterable,
     &'a I: IntoIterator<Item = &'a I::Item>,
 {
-    type Collection<U> = I::Collection<U>;
+    type CC<U> = I::CC<U>;
 }
 
-
-
-
-
-
-
-
-
+impl<'a, K, V, IM> IterableMap<&'a K, &'a V> for &'a IM
+    where
+        IM: IterableMap<K, V>,
+        &'a IM: Iterable<Item = (&'a K, &'a V)>,
+{
+    type CCMap<X, Y> = IM::CCMap<X, Y>;
+}
 
 // pub struct IterableWrap<'a, I: Iterable + 'a> {
 //     iterable: &'a I
