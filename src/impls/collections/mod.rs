288933c240364b3223206ed81a5a9d8dc41288ea
nooberfsh <nooberfsh@gmail.com>
2020-09-19 08:38:52 +0800

    create collections module

---
+++ src/impls/collections/mod.rs
@@ -0,0 +1,3 @@
+pub mod hash_map;
+pub mod hash_set;
+pub mod vec;
--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -1,13 +1,9 @@
 mod reference;
 mod slice;
 mod with_filter;
-mod vec;
-mod hash_set;
-mod hash_map;
+mod collections;
 
 pub use reference::*;
 pub use slice::*;
 pub use with_filter::*;
-pub use vec::*;
-pub use hash_set::*;
-pub use hash_map::*;
+pub use collections::*;
