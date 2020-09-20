81d3b8521107d71e79e2ef29949ca8387951093a
nooberfsh <nooberfsh@gmail.com>
2020-09-20 15:16:52 +0800

    add methods: last, nth, step_by, chain, zip, foreach to Iterable

--- src/lib.rs
+++ src/lib.rs
@@ -22,12 +22,57 @@ pub trait Iterable: Consumer {
         self.into_iter().count()
     }
 
-    fn filter_map<U>(self, f: impl Fn(Self::Item) -> Option<U>) -> Self::CC<U>
+    fn last(self) -> Option<Self::Item>
+    where
+        Self: Sized,
+    {
+        self.into_iter().last()
+    }
+
+    fn nth(self, n: usize) -> Option<Self::Item>
+    where
+        Self: Sized,
+    {
+        self.into_iter().nth(n)
+    }
+
+    fn step_by(self, step: usize) -> Self::C
+    where
+        Self: Sized,
+        Self::C: Producer<Self::Item>,
+    {
+        Self::C::from_iter(self.into_iter().step_by(step))
+    }
+
+    fn chain(self, other: impl Iterable<Item = Self::Item>) -> Self::C
+    where
+        Self: Sized,
+        Self::C: Producer<Self::Item>,
+    {
+        Self::C::from_iter(self.into_iter().chain(other.into_iter()))
+    }
+
+    fn zip<E>(self, other: impl Iterable<Item=E>) -> Self::CC<(Self::Item, E)>
+    where
+        Self: Sized,
+        Self::CC<(Self::Item, E)>: Producer<(Self::Item, E)>,
+    {
+        Self::CC::<(Self::Item, E)>::from_iter(self.into_iter().zip(other.into_iter()))
+    }
+
+    fn map<U>(self, f: impl Fn(Self::Item) -> U) -> Self::CC<U>
     where
         Self: Sized,
         Self::CC<U>: Producer<U>,
     {
-        Self::CC::<U>::from_iter(self.into_iter().filter_map(f))
+        Self::CC::<U>::from_iter(self.into_iter().map(f))
+    }
+
+    fn foreach(self, f: impl Fn(Self::Item))
+    where
+        Self: Sized,
+    {
+        self.into_iter().for_each(f)
     }
 
     fn filter(self, f: impl Fn(&Self::Item) -> bool) -> Self::C
@@ -38,12 +83,12 @@ pub trait Iterable: Consumer {
         Self::C::from_iter(self.into_iter().filter(f))
     }
 
-    fn map<U>(self, f: impl Fn(Self::Item) -> U) -> Self::CC<U>
+    fn filter_map<U>(self, f: impl Fn(Self::Item) -> Option<U>) -> Self::CC<U>
     where
         Self: Sized,
         Self::CC<U>: Producer<U>,
     {
-        Self::CC::<U>::from_iter(self.into_iter().map(f))
+        Self::CC::<U>::from_iter(self.into_iter().filter_map(f))
     }
 
     fn with_filter<F: Fn(&Self::Item) -> bool>(self, f: F) -> WithFilter<Self, F>
