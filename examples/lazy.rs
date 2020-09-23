4fdb70113850bb6b62c6c39856cbc12a5cc3a146
nooberfsh <nooberfsh@gmail.com>
2020-09-23 09:37:25 +0800

    add some examples

---
+++ examples/lazy.rs
@@ -0,0 +1,8 @@
+use iterable::*;
+
+fn main() {
+    let int_vec = vec![1,2,3];
+    // with_filter is a lazy combinator
+    let string_vec = int_vec.with_filter(|x| x > &1).map(|x| format!("{}", x));
+    string_vec.foreach(|x| println!("{}", x));
+}
