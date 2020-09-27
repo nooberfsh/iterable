40d24b7573826463ec9f5bb67473f246e05bf09c
nooberfsh <nooberfsh@gmail.com>
2020-09-27 18:29:26 +0800

    lazy_flat_map::Iter use lazy_flatten::Iter's iterator.

--- src/lazy/lazy_flat_map.rs
+++ src/lazy/lazy_flat_map.rs
@@ -1,4 +1,4 @@
-use crate::{Iterable, Consumer};
+use crate::{Iterable, Consumer, FlattenIter};
 
 #[must_use = "iterable adaptors are lazy and do nothing unless consumed"]
 #[derive(Debug, Clone)]
@@ -24,37 +24,35 @@ where
     T: Consumer,
 {
     type Item = <T as Consumer>::Item;
-    type IntoIter = Iter<I::IntoIter, F, T>;
+    type IntoIter = FlattenMapIter<I::IntoIter, F, T>;
     fn consume(self) -> Self::IntoIter {
-        new_iter(self.iterable, self.f)
+        new_flat_map_iter(self.iterable, self.f)
     }
 }
 
-pub struct Iter<I, F, T>
+pub struct FlattenMapIter<I, F, T>
 where
     I: Iterator,
     F: Fn(I::Item) -> T,
     T: Consumer,
 {
-    iter: I,
-    f: F,
-    inner: Option<<T as Consumer>::IntoIter>,
+    inner: FlattenIter<std::iter::Map<I, F>>
 }
 
-fn new_iter<C, F, T>(c: C, f: F) -> Iter<C::IntoIter, F ,T>
+fn new_flat_map_iter<C, F, T>(c: C, f: F) -> FlattenMapIter<C::IntoIter, F ,T>
 where
     C: Consumer,
     F: Fn(C::Item) -> T,
     T: Consumer,
 {
-    Iter {
-        f,
-        iter: c.consume(),
+    let inner = FlattenIter {
+        iter: c.consume().map(f),
         inner: None,
-    }
+    };
+    FlattenMapIter {inner}
 }
 
-impl<I, F, T> Iterator for Iter<I, F, T>
+impl<I, F, T> Iterator for FlattenMapIter<I, F, T>
 where
     I: Iterator,
     F: Fn(I::Item) -> T,
@@ -62,29 +60,9 @@ where
 {
     type Item = T::Item;
     fn next(&mut self) -> Option<Self::Item> {
-        match self.inner.take() {
-            None => {
-                match self.iter.next() {
-                    None => None,
-                    Some(d) => {
-                        self.inner = Some((self.f)(d).consume());
-                        self.next()
-                    }
-                }
-            }
-            Some(mut i) =>  {
-                match i.next() {
-                    None => self.next(),
-                    d =>  {
-                        self.inner = Some(i);
-                        d
-                    }
-                }
-            }
-        }
+        self.inner.next()
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -100,21 +78,21 @@ mod tests {
 
     #[test]
     fn test_iter() {
-        let a = new_iter(vec![1,2,3], |x| vec![x, 1]);
+        let a = new_flat_map_iter(vec![1, 2, 3], |x| vec![x, 1]);
         let res: Vec<_> = a.collect();
         assert_eq!(res, vec![1,1,2,1,3,1])
     }
 
     #[test]
     fn test_iter2() {
-        let a = new_iter(vec![1,2,3], |x| if x == 2 { vec![] } else { vec![x, 1] });
+        let a = new_flat_map_iter(vec![1, 2, 3], |x| if x == 2 { vec![] } else { vec![x, 1] });
         let res: Vec<_> = a.collect();
         assert_eq!(res, vec![1,1,3,1])
     }
 
     #[test]
     fn test_iter3() {
-        let a = new_iter(vec![], |x: i32| vec![x, 1]);
+        let a = new_flat_map_iter(vec![], |x: i32| vec![x, 1]);
         let res: Vec<_> = a.collect();
         assert_eq!(res, vec![]);
     }
--- src/lazy/lazy_flatten.rs
+++ src/lazy/lazy_flatten.rs
@@ -21,33 +21,33 @@ where
     I::Item: Consumer,
 {
     type Item = <I::Item as Consumer>::Item;
-    type IntoIter = Iter<I::IntoIter>;
+    type IntoIter = FlattenIter<I::IntoIter>;
     fn consume(self) -> Self::IntoIter {
-        new_iter(self.iterable)
+        new_flatten_iter(self.iterable)
     }
 }
 
-pub struct Iter<I>
+pub struct FlattenIter<I>
 where
     I: Iterator,
     I::Item: Consumer,
 {
-    iter: I,
-    inner: Option<<I::Item as Consumer>::IntoIter>,
+    pub (super) iter: I,
+    pub (super) inner: Option<<I::Item as Consumer>::IntoIter>,
 }
 
-pub (super) fn new_iter<C>(c: C) -> Iter<C::IntoIter>
+pub (super) fn new_flatten_iter<C>(c: C) -> FlattenIter<C::IntoIter>
 where
     C: Consumer,
     C::Item: Consumer,
 {
-    Iter {
+    FlattenIter {
         iter: c.consume(),
         inner: None,
     }
 }
 
-impl<I> Iterator for Iter<I>
+impl<I> Iterator for FlattenIter<I>
 where
     I: Iterator,
     I::Item: Consumer,
@@ -87,5 +87,27 @@ mod tests {
         let v = vec![[1,2], [3,4]];
         let res = collect(v.lazy_flatten());
         assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_iter() {
+        let a = new_flatten_iter(vec![[1,1], [2,1], [3,1]]);
+        let res: Vec<_> = a.collect();
+        assert_eq!(res, vec![1,1,2,1,3,1])
+    }
+
+    #[test]
+    fn test_iter2() {
+        let a = new_flatten_iter(vec![vec![1,1], vec![], vec![3,1]]);
+        let res: Vec<_> = a.collect();
+        assert_eq!(res, vec![1,1,3,1])
+    }
+
+    #[test]
+    fn test_iter3() {
+        let e: Vec<[i32; 10]> = vec![];
+        let a = new_flatten_iter(e);
+        let res: Vec<_> = a.collect();
+        assert_eq!(res, vec![]);
     }
 }
