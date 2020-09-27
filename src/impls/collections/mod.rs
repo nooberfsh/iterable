a44294da43dea194e7e46fd23b45a077306eb6dc
nooberfsh <nooberfsh@gmail.com>
2020-09-27 10:45:31 +0800

    modify vec module path

--- src/impls/collections/mod.rs
+++ src/impls/collections/mod.rs
@@ -1,6 +1,5 @@
 pub mod hash_map;
 pub mod hash_set;
-pub mod vec;
 pub mod btree_set;
 pub mod btree_map;
 pub mod binary_heap;
--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -4,6 +4,7 @@ mod collections;
 mod string;
 mod str;
 mod array;
+mod vec;
 
 pub use self::reference::*;
 pub use self::slice::*;
@@ -11,3 +12,4 @@ pub use self::collections::*;
 pub use self::string::*;
 pub use self::str::*;
 pub use self::array::*;
+pub use self::vec::*;
