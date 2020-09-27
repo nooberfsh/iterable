a65226cbb5e1763a9662b7bbfafc014da1ff4673
nooberfsh <nooberfsh@gmail.com>
2020-09-27 13:33:51 +0800

    refine tests for LazyFilter

--- src/lazy/lazy_filter.rs
+++ src/lazy/lazy_filter.rs
@@ -31,46 +31,12 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::lazy::collect;
 
     #[test]
-    fn test_c() {
+    fn smoke() {
         let v = vec![1, 2, 3];
-        let res = v.lazy_filter(|i| i > &1).filter(|i| i > &2);
-        assert_eq!(res, vec![3]);
-    }
-
-    #[test]
-    fn test_f() {
-        let v = [1, 2, 3];
-        let res = v.lazy_filter(|i| i > &1).filter(|i| i > &2);
-        assert_eq!(res, vec![3]);
-    }
-
-    #[test]
-    fn test_cc() {
-        let v = vec![1, 2, 3];
-        let res = v.lazy_filter(|i| i > &1).map(|i| i.to_string());
-        assert_eq!(res, vec!["2".to_string(), "3".to_string()]);
-    }
-
-    #[test]
-    fn test_cf() {
-        let v = [1, 2, 3];
-        let res = v.lazy_filter(|i| i > &1).map(|i| i.to_string());
-        assert_eq!(res, vec!["2".to_string(), "3".to_string()]);
-    }
-
-    #[test]
-    fn test_c_r() {
-        let v = vec![1, 2, 3];
-        let res = (&v).lazy_filter(|i| i > &&1).filter(|i| i > &&2);
-        assert_eq!(res, vec![&3]);
-    }
-
-    #[test]
-    fn test_cc_r() {
-        let v = vec![1, 2, 3];
-        let res = (&v).lazy_filter(|i| i > &&1).map(|i| i.to_string());
-        assert_eq!(res, vec!["2".to_string(), "3".to_string()]);
+        let res = collect(v.lazy_filter(|i| i > &1));
+        assert_eq!(res, vec![2, 3]);
     }
 }
--- src/lazy/mod.rs
+++ src/lazy/mod.rs
@@ -1,3 +1,13 @@
 mod lazy_filter;
 
 pub use lazy_filter::*;
+
+
+// only used for test lazy combinator
+#[cfg(test)]
+fn collect<I: crate::Iterable>(i: I) -> I::F
+where I::F: crate::Producer<I::Item>
+{
+    use crate::Producer;
+    I::F::produce(i.consume())
+}
