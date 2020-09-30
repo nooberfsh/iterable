b6245233aa9ab940ffb092142db0a46c0f73a648
nooberfsh <nooberfsh@gmail.com>
2020-09-30 11:39:23 +0800

    refine chain test

--- tests/lazy.rs
+++ tests/lazy.rs
@@ -6,6 +6,8 @@ fn chain() {
     let s = v
         .lazy_filter(|x| x > &1)
         .lazy_map(|x| x.to_string())
+        .lazy_rev()
+        .lazy_rev()
         .rev();
     assert_eq!(s, vec!["3".to_string(), "2".to_string()]);
 }
